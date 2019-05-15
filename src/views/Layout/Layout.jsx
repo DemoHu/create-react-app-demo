e0025b2e386149942d48e495b9d936327b3262ec
siwen <623746556@qq.com>
2019-05-15 15:55:04 +0800

    使用lazy

--- src/views/Layout/Layout.jsx
+++ src/views/Layout/Layout.jsx
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
 import { Route, Switch, Redirect } from 'react-router-dom';
-
-import Home from '@/views/Home/Home';
-import ApiDoc from '@/views/ApiDoc/ApiDoc';
-import SdkDownload from '@/views/SdkDownload/SdkDownload';
 import { Layout } from 'antd'
 const { Header, Content, Footer } = Layout;
+
+const Home = React.lazy(() => import('@/views/Home/Home'))
+const ApiDoc = React.lazy(() => import('@/views/ApiDoc/ApiDoc'))
+const SdkDownload = React.lazy(() => import('@/views/SdkDownload/SdkDownload'))
 export default class LayoutBox extends Component {
   constructor(props) {
     super(props);
@@ -18,12 +18,14 @@ export default class LayoutBox extends Component {
           <Header>Header</Header>
           <Content>
             <div className="main">
-              <Switch>
-                <Redirect exact to="/home" from="/" />
-                <Route path="/home" component={ Home }></Route>
-                <Route path="/api_doc" component={ ApiDoc }></Route>
-                <Route path="sdk_download" component={ SdkDownload }></Route>
-              </Switch>
+              <React.Suspense fallback={<div>loading...</div>}>
+                <Switch>
+                  <Redirect exact to="/home" from="/" />
+                  <Route path="/home" component={ Home }></Route>
+                  <Route path="/api_doc" component={ ApiDoc }></Route>
+                  <Route path="/sdk_download" component={ SdkDownload }></Route>
+                </Switch>
+              </React.Suspense>
             </div>
           </Content>
           <Footer>Footer</Footer>
